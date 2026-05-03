0db3bd315c338ef4e60cedd48983f8c8da01102c
vabhi2024 <vabhi2024@gmail.com>
2026-05-03 18:43:22 +0530

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,7 @@
+function openPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// Drop down Navbar
+function openMenu() {
+    document.getElementById("full-screen-menu").classList.add("active");
+}
+
+function closeMenu() {
+    document.getElementById("full-screen-menu").classList.remove("active");
+}
