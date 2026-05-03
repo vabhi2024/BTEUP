0613223321dfcd04ab98d84d16567d5b1b2b7de2
vabhi2024 <vabhi2024@gmail.com>
2026-05-03 18:46:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(reg => {
+      console.log("Service Worker registered:", reg);
+    })
+    .catch(err => {
+      console.log("Service Worker registration failed:", err);
+    });
+}
